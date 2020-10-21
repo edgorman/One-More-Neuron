3683d73edad04115ebcdd35ae998049a49c0c0de
Edward Gorman <ejgorman@gmail.com>
2020-10-21 21:23:16 +0100

    Add RL file with comments

---
+++ src/reinforcement-learning.py
@@ -0,0 +1,10 @@
+""" Reinforcement Learning file
+"""
+from .agent import Agent
+
+
+class ReinforcementLearning(Agent):
+    """ This is the Reinforcement Learning class """
+
+    def __init__(self):
+        pass
